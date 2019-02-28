9af71830a38c20dbd5db9acb50f151466ff26266
Accalia Elementia <accalia@elementia.me>
2019-02-28 00:42:29 +0000

    refactor client side js

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,295 +1,264 @@
 'use sanity'
 /* global $, picturereaderdata */
 
-$('#mainMenuItem').click(() => {
-  $('#mainMenu').toggle()
-})
-
-$('input[name=ShowUnseenOnly]').change(() => {
-  window.localStorage['ComicReader-ShowUnseenOnly'] = $('input[name=ShowUnseenOnly]').prop('checked')
-})
-
-const showUnseenOnly = () => window.localStorage['ComicReader-ShowUnseenOnly'] === 'true'
-
-$('input[name=ShowUnseenOnly]').prop('checked', showUnseenOnly())
-
-$('#bookmarks button.remove').on('click', function () {
-  const self = $(this).closest('.card')
-  $.ajax({
-    method: 'DELETE',
-    url: self.data('link'),
-    complete: () => self.remove()
-  })
-})
-
-if (!picturereaderdata.pictures.length) {
-  $('#folders-tab').tab('show')
-  $('#mainMenu').show()
-} else {
-  $('#mainImage img').show()
-}
-
-const setBookmark = () => {
-  $.post('/api/bookmarks/add', { path: picturereaderdata.pictures[imageIdx].path })
-  $('#mainMenu').hide()
-}
-$('.action-block .action-bookmark').click(setBookmark)
-
-let imageIdx = 0
-picturereaderdata.pictures.forEach((f, i) => {
-  if (f.path === picturereaderdata.current) {
-    imageIdx = i
-  }
-})
-
-const nope = () => {
+$('#mainImage').on('error', () => {
   $('.navbar').addClass('ease').removeClass('bg-light').addClass('bg-danger')
   setTimeout(() => $('.navbar').addClass('bg-light'), 500)
   setTimeout(() => $('.navbar').removeClass('ease').removeClass('bg-danger'), 1000)
-}
-
-const loadImage = () => {
-  const pic = picturereaderdata.pictures[imageIdx]
-  if (!pic) {
-    return
-  }
-  pic.seen = true
-  $('title').text(picturereaderdata.name)
-  $('nav .title').text(picturereaderdata.name)
-  $('.status-bar .center').text(pic.name)
-  $('.status-bar .left').text(`(${imageIdx + 1}/${picturereaderdata.pictures.length})`)
-  $.post('/api/navigate/latest', { path: pic.path })
-  $('#mainImage img').attr('src', pic.path)
-  $('#mainMenu').hide()
-}
-loadImage()
-
-$('#pictures .card').click(function () {
-  imageIdx = +$(this).data('index')
-  loadImage()
 })
 
-const navigation = {
-  markRead: () => {
+function MainMenu () {
+  $('#mainMenu').on('showMenu', () => {
+    $('#mainMenu').show()
+  })
+  $('#mainMenu').on('hideMenu', () => {
+    if (!picturereaderdata.pictures.length) {
+      return
+    }
+    $('#mainMenu').hide()
+  })
+  $('#mainMenu').on('toggleMenu', () => {
+    if ($('#mainMenu').is(':visible')) {
+      $('#mainMenu').trigger('hideMenu')
+    } else {
+      $('#mainMenu').trigger('showMenu')
+    }
+  })
+  if (picturereaderdata.pictures.length && picturereaderdata.pictures.every(pic => pic.seen)) {
+    $('#mainMenu').trigger('showMenu')
+  }
+  if (!picturereaderdata.pictures.length) {
+    $('#mainMenu').trigger('showMenu')
+  } else {
+    $('#mainImage img').show()
+  }
+  $('#mainMenuItem').click(() => $('#mainMenu').trigger('toggleMenu'))
+  $('input[name=ShowUnseenOnly]').change(() => {
+    window.localStorage['ComicReader-ShowUnseenOnly'] = $('input[name=ShowUnseenOnly]').prop('checked')
+  })
+  $('input[name=ShowUnseenOnly]').prop('checked', window.localStorage['ComicReader-ShowUnseenOnly'] === 'true')
+}
+MainMenu()
+
+function MainImage () {
+  const isUnseenOnly = () => window.localStorage['ComicReader-ShowUnseenOnly'] === 'true'
+  let after = () => {
+    after = () => $('#mainMenu').trigger('hideMenu')
+  }
+  const pics = picturereaderdata.pictures
+  let index = 0
+  pics.forEach((f, i) => {
+    if (f.path === picturereaderdata.current) {
+      index = i
+    }
+  })
+  const loadImage = pic => {
+    if (!pic) {
+      return
+    }
+    pic.seen = true
+    $('title').text(picturereaderdata.name)
+    $('nav .title').text(picturereaderdata.name)
+    $('.status-bar .center').text(pic.name)
+    $('.status-bar .left').text(`(${index + 1}/${picturereaderdata.pictures.length})`)
+    $.post('/api/navigate/latest', { path: pic.path })
+    $('#mainImage img').attr('src', pic.path).data('path', pic.path).data('index', index)
+    after()
+  }
+  const changeImage = (isValid, action) => {
+    if (isValid) {
+      action()
+      loadImage(pics[index])
+    } else {
+      $('#mainImage').trigger('error')
+    }
+  }
+  loadImage(pics[index])
+  $('body').on('randomImage', () => changeImage(true, () => {
+    index = Math.floor(Math.random() * pics.length)
+  })).on('first', () => changeImage(index > 0, () => {
+    index = 0
+  })).on('last', () => changeImage(index < pics.length - 1, () => {
+    index = pics.length - 1
+  })).on('previousImage', () => changeImage(index > 0, () => {
+    index--
+  })).on('nextImage', () => changeImage(index < pics.length - 1, () => {
+    index++
+  })).on('previousUnseen', () => changeImage(pics.some((e, i) => i < index && !e.seen), () => {
+    index = pics.reduce((acc, pic, i) => !pic.seen && i < index ? Math.max(acc, i) : acc, 0)
+  })).on('nextUnseen', () => changeImage(pics.some((e, i) => i > index && !e.seen), () => {
+    index = pics.reduce((acc, pic, i) => !pic.seen && i > index ? Math.min(acc, i) : acc, Infinity)
+  })).on('next', () => {
+    if (isUnseenOnly()) {
+      $('#mainImage').trigger('nextUnseen')
+    } else {
+      $('#mainImage').trigger('nextImage')
+    }
+  }).on('previous', () => {
+    if (isUnseenOnly()) {
+      $('#mainImage').trigger('previousUnseen')
+    } else {
+      $('#mainImage').trigger('previousImage')
+    }
+  }).on('loadIndex', (_, i) => changeImage(true, () => {
+    index = +i
+  }))
+}
+MainImage()
+
+function Bookmarks () {
+  $('#bookmarks button.remove').on('click', function () {
+    const self = $(this).closest('.card')
+    $.ajax({
+      method: 'DELETE',
+      url: self.data('link'),
+      complete: () => self.remove()
+    })
+  })
+  $('body').on('bookmark', () => {
+    $.post('/api/bookmarks/add', { path: $('#mainImage img').data('path') })
+    $('#mainMenu').trigger('hideMenu')
+  }).on('markAllSeen', () => {
     $.ajax({
       method: 'POST',
       url: '/api/mark/read',
       data: { path: picturereaderdata.path },
       complete: () => window.location.reload(true)
     })
-  },
-  markUnRead: () => {
+  }).on('markAllUnseen', () => {
     $.ajax({
       method: 'POST',
       url: '/api/mark/unread',
       data: { path: picturereaderdata.path },
       complete: () => window.location.reload(true)
     })
-  },
-  firstImage: () => {
-    imageIdx = 0
-    loadImage()
-  },
-  nextImage: () => {
-    if (imageIdx < picturereaderdata.pictures.length - 1) {
-      imageIdx++
-      loadImage()
-    } else {
-      nope()
-    }
-  },
-  randomImage: () => {
-    imageIdx = Math.floor(Math.random() * picturereaderdata.pictures.length)
-    loadImage()
-  },
-  prevImage: () => {
-    if (imageIdx > 0) {
-      imageIdx--
-      loadImage()
-    } else {
-      nope()
-    }
-  },
-  lastImage: () => {
-    imageIdx = picturereaderdata.pictures.length - 1
-    loadImage()
-  },
-  nextFolder: () => {
+  })
+}
+Bookmarks()
+
+function Folders () {
+  $('body').on('nextFolder', () => {
     if (picturereaderdata.nextFolder) {
       window.location = picturereaderdata.nextFolder
     } else {
-      nope()
+      $('#mainImage').trigger('error')
     }
-  },
-  previousFolder: () => {
+  }).on('previousFolder', () => {
     if (picturereaderdata.previousFolder) {
       window.location = picturereaderdata.previousFolder
     } else {
-      nope()
+      $('#mainImage').trigger('error')
     }
-  },
-  previousUnseen: () => {
-    let idx = imageIdx
-    while (idx > 0) {
-      idx--
-      if (!picturereaderdata.pictures[idx].seen) {
-        imageIdx = idx
-        loadImage()
-        return
-      }
-    }
-    nope()
-  },
-  nextUnseen: () => {
-    let idx = imageIdx
-    while (idx < picturereaderdata.pictures.length - 1) {
-      idx++
-      if (!picturereaderdata.pictures[idx].seen) {
-        imageIdx = idx
-        loadImage()
-        return
-      }
-    }
-    nope()
-  },
-  fullscreen: () => {
+  }).on('parentFolder', () => {
+    window.location = `/show${picturereaderdata.parent}`
+  })
+}
+Folders()
+
+function Navigation () {
+  $('.action-button').click(function () {
+    $(this).trigger($(this).data('action'))
+  })
+  $('#pictures .card').click(function () {
+    $('#mainImage').trigger('loadIndex', [$(this).data('index')])
+  })
+  $('body').on('fullscreen', () => {
     if (!window.document.webkitFullscreenElement) {
-      window.document.body.webkitRequestFullscreen({
-        navigationUI: false
-      })
+      window.document.body.webkitRequestFullscreen()
     } else {
       window.document.webkitExitFullscreen()
     }
-  },
-  parentfolder: () => {
-    window.location = `/show${picturereaderdata.parent}`
-  }
-}
-document.onkeyup = (evt) => {
-  if ($('#mainMenu').is(':visible')) {
-    return
-  }
-  var key = (evt.ctrlKey ? '<CTRL>' : '') +
-      (evt.altKey ? '<ALT>' : '') +
-      (evt.shiftKey ? '<SHIFT>' : '') +
-      evt.key.toUpperCase()
-  var action = ''
-  switch (key) {
-    case 'ARROWUP':
-      $('#mainMenu').show()
-      break
-    case 'ARROWdown':
-      $('#mainMenu').hide()
-      break
-    case 'ARROWRIGHT':
-      action = showUnseenOnly() ? 'nextUnseen' : 'nextImage'
-      break
-    case 'ARROWLEFT':
-      action = showUnseenOnly() ? 'previousUnseen' : 'prevImage'
-      break
-  }
-  (navigation[action] || function () {})()
-}
-$('.action-block .action-first').click(navigation.firstImage)
-$('.action-block .action-previous').click(navigation.prevImage)
-$('.action-block .action-random').click(navigation.randomImage)
-$('.action-block .action-next').click(navigation.nextImage)
-$('.action-block .action-last').click(navigation.lastImage)
-
-$('.action-block .action-nextfolder').click(navigation.nextFolder)
-$('.action-block .action-previousfolder').click(navigation.previousFolder)
-$('.action-block .action-nextunseen').click(navigation.nextUnseen)
-$('.action-block .action-previousunseen').click(navigation.previousUnseen)
-$('.action-block .action-fullscreen').click(navigation.fullscreen)
-$('.action-block .action-parentfolder').click(navigation.parentfolder)
-$('.action-block .action-markallseen').click(navigation.markRead)
-$('.action-block .action-markallunseen').click(navigation.markUnRead)
-
-const limit = Math.tan(45 * 1.5 / 180 * Math.PI)
-
-let pendingTouch = null
-$('#mainImage').on('touchstart', event => {
-  if (!pendingTouch) {
-    pendingTouch = event.originalEvent.changedTouches[0]
-  }
-}).on('touchend', event => {
-  const touch = event.originalEvent.changedTouches[0]
-  if (pendingTouch && pendingTouch.identifier === touch.identifier) {
-    console.log(window.visualViewport.scale, initialScale, pendingTouch, touch)
-    if (!window.visualViewport || window.visualViewport.scale <= initialScale) {
-      handleGesture(pendingTouch, touch)
+  })
+  document.onkeyup = (evt) => {
+    if ($('#mainMenu').is(':visible')) {
+      return
     }
-    pendingTouch = null
+    var key = (evt.ctrlKey ? '<CTRL>' : '') +
+        (evt.altKey ? '<ALT>' : '') +
+        (evt.shiftKey ? '<SHIFT>' : '') +
+        evt.key.toUpperCase()
+    switch (key) {
+      case 'ARROWUP':
+        $('#mainMenu').trigger('showMenu')
+        break
+      case 'ARROWDOWN':
+        $('#mainMenu').trigger('hideMenu')
+        break
+      case 'ARROWRIGHT':
+        $('#mainImage').trigger('next')
+        break
+      case 'ARROWLEFT':
+        $('#mainImage').trigger('previous')
+        break
+    }
   }
-}).on('click', evt => {
-  if ($('#mainMenu').is(':visible')) {
-    $('#mainMenu').hide()
-    return
-  }
-  if (window.visualViewport && window.visualViewport.scale > initialScale) {
-    return
-  }
-  const pageWidth = window.innerWidth || document.body.clientWidth
-  const x = evt.pageX
-  if (x < pageWidth / 3) {
-    if (showUnseenOnly()) {
-      navigation.previousUnseen()
+
+  const limit = Math.tan(45 * 1.5 / 180 * Math.PI)
+
+  let pendingTouch = null
+  $('#mainImage').on('touchstart', event => {
+    if (!pendingTouch) {
+      pendingTouch = event.originalEvent.changedTouches[0]
+    }
+  }).on('touchend', event => {
+    const touch = event.originalEvent.changedTouches[0]
+    if (pendingTouch && pendingTouch.identifier === touch.identifier) {
+      console.log(window.visualViewport.scale, initialScale, pendingTouch, touch)
+      if (!window.visualViewport || window.visualViewport.scale <= initialScale) {
+        handleGesture(pendingTouch, touch)
+      }
+      pendingTouch = null
+    }
+  }).on('click', evt => {
+    if ($('#mainMenu').is(':visible')) {
+      $('#mainMenu').trigger('hideMenu')
+      return
+    }
+    if (window.visualViewport && window.visualViewport.scale > initialScale) {
+      return
+    }
+    const pageWidth = window.innerWidth || document.body.clientWidth
+    const x = evt.pageX
+    if (x < pageWidth / 3) {
+      $('#mainImage').trigger('previous')
+    } else if (x < pageWidth * 2 / 3) {
+      $('#mainMenu').trigger('showMenu')
     } else {
-      navigation.prevImage()
+      $('#mainImage').trigger('next')
     }
-  } else if (x < pageWidth * 2 / 3) {
-    $('#mainMenu').show()
-  } else {
-    if (showUnseenOnly()) {
-      navigation.nextUnseen()
-    } else {
-      navigation.nextImage()
-    }
-  }
-})
+  })
 
-const initialScale = window.visualViewport ? window.visualViewport.scale : 1
+  const initialScale = window.visualViewport ? window.visualViewport.scale : 1
 
-const handleGesture = (startTouch, endTouch) => {
-  const pageWidth = window.innerWidth || document.body.clientWidth
-  const treshold = Math.max(25, Math.floor(0.02 * (pageWidth)))
-  const swipeX = endTouch.screenX - startTouch.screenX
-  const swipeY = endTouch.screenY - startTouch.screenY
-  const swipeVertical = Math.abs(swipeX / swipeY) <= limit
-  const swipeHorizontal = Math.abs(swipeY / swipeX) <= limit
-  if (Math.abs(swipeX) > treshold || Math.abs(swipeY) > treshold) {
-    if (swipeHorizontal) {
-      if (swipeX > 0) {
-        if (showUnseenOnly()) {
-          navigation.previousUnseen()
+  const handleGesture = (startTouch, endTouch) => {
+    const pageWidth = window.innerWidth || document.body.clientWidth
+    const treshold = Math.max(25, Math.floor(0.02 * (pageWidth)))
+    const swipeX = endTouch.screenX - startTouch.screenX
+    const swipeY = endTouch.screenY - startTouch.screenY
+    const swipeVertical = Math.abs(swipeX / swipeY) <= limit
+    const swipeHorizontal = Math.abs(swipeY / swipeX) <= limit
+    if (Math.abs(swipeX) > treshold || Math.abs(swipeY) > treshold) {
+      if (swipeHorizontal) {
+        if (swipeX > 0) {
+          $('#mainImage').trigger('previous')
         } else {
-          navigation.prevImage()
+          $('#mainImage').trigger('next')
         }
+      }
+      if (swipeVertical) {
+        if (swipeY > 0) {
+          $('#mainMenu').trigger('showMenu')
+        }
+      }
+    } else {
+      if (endTouch.screenX < pageWidth / 3) {
+        // navigation.prevImage()
+      } else if (endTouch.screenX < pageWidth * 2 / 3) {
+        $('#mainMenu').trigger('showMenu')
       } else {
-        if (showUnseenOnly()) {
-          navigation.nextUnseen()
-        } else {
-          navigation.nextImage()
-        }
+        // navigation.nextImage()
       }
-    }
-    if (swipeVertical) {
-      if (swipeY > 0) {
-        $('#mainMenu').show()
-      }
-    }
-  } else {
-    if (endTouch.screenX < pageWidth / 3) {
-      // navigation.prevImage()
-    } else if (endTouch.screenX < pageWidth * 2 / 3) {
-      $('#mainMenu').show()
-    } else {
-      // navigation.nextImage()
     }
   }
 }
-
-if (picturereaderdata.pictures.length && picturereaderdata.pictures.every(pic => pic.seen)) {
-  $('#folders-tab').tab('show')
-  $('#mainMenu').show()
-}
+Navigation()
