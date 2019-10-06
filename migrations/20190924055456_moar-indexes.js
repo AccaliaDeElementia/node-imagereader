c38f7bb3daf4eab199fdca6ababffdd692fcb4ab
Accalia Elementia <accalia@elementia.me>
2019-10-06 11:27:49 +0000

    begin refactor for code climate part 2

--- migrations/20190924055456_moar-indexes.js
+++ migrations/20190924055456_moar-indexes.js
@@ -1,36 +1,24 @@
 
-exports.up = function (knex) {
-  return knex.schema.alterTable('perceptualFingerprint', table => {
-    ['hexHashA', 'hexHashB', 'hexHashC', 'hexHashD'].forEach(i => table.dropIndex(i))
-    table.index([
-      'hexHashA',
-      'hexHashB',
-      'hexHashC',
-      'hexHashD'
+const doIndexes = (knex, on, off) => knex.schema.alterTable('perceptualFingerprint', table => {
+  ['hexHashA', 'hexHashB', 'hexHashC', 'hexHashD'].forEach(i => table[off](i))
+  table[on]([
+    'hexHashA',
+    'hexHashB',
+    'hexHashC',
+    'hexHashD'
+  ])
+})
+  .then(() => knex.schema.alterTable('perceptualComparison', table => {
+    table[on]([
+      'left',
+      'right'
     ])
-  })
-    .then(() => knex.schema.alterTable('perceptualComparison', table => {
-      table.index([
-        'left',
-        'right'
-      ])
-    }))
+  }))
+
+exports.up = function (knex) {
+  return doIndexes(knex, 'index', 'dropIndex')
 }
 
 exports.down = function (knex) {
-  return knex.schema.alterTable('perceptualFingerprint', table => {
-    ['hexHashA', 'hexHashB', 'hexHashC', 'hexHashD'].forEach(i => table.index(i))
-    table.dropIndex([
-      'hexHashA',
-      'hexHashB',
-      'hexHashC',
-      'hexHashD'
-    ])
-  })
-    .then(() => knex.schema.alterTable('perceptualComparison', table => {
-      table.dropIndex([
-        'left',
-        'right'
-      ])
-    }))
+  return doIndexes(knex, 'dropIndex', 'index')
 }
